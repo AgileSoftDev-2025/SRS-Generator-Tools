6b5529b6340e1599c833eec9dba862cd67c51fd2
ElgaIgnafia <elga.ignafia1905@gmail.com>
2025-10-26 10:16:49 +0700

    Mengupdate file urls.py

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('previous/', views.previous_page, name='previous_page'),
+    path('input_informasi_tambahan/', views.input_informasi_tambahan, name='input_informasi_tambahan'),
+    path('save_use_case_spec/<int:feature_id>/', views.save_use_case_spec, name='save_use_case_spec'),
+    path('use_case_spec/', views.use_case_spec, name='use_case_spec'),
 ]
