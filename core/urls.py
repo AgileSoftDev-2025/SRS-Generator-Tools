0c57455d01cb34cbb2ccdebc8cabda97f736c37e
ElgaIgnafia <elga.ignafia1905@gmail.com>
2025-10-26 10:45:31 +0700

    update file urls.py

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', include('main.urls')),
     path('previous/', views.previous_page, name='previous_page'),
     path('input_informasi_tambahan/', views.input_informasi_tambahan, name='input_informasi_tambahan'),
+    path('input_informasi_tambahan/use_case_spec.html', views.use_case_spec, name='use_case_spec'),
     path('save_use_case_spec/<int:feature_id>/', views.save_use_case_spec, name='save_use_case_spec'),
     path('use_case_spec/', views.use_case_spec, name='use_case_spec'),
 ]
