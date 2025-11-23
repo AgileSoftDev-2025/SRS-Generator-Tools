5b667e5caadce32b8d72d3e87f9a9959c34483a9
bungacal <bungacalista1412@gmail.com>
2025-11-23 21:48:12 +0700

    Add register url

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,7 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('user-story/', views.user_story, name='user_story'),
