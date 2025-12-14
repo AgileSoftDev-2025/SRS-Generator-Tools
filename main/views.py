73483542b2edc2e25e178d5d32840c0a3061f2fa
bungacal <bungacalista1412@gmail.com>
2025-12-14 18:21:37 +0700

    Fix login and register session

--- main/views.py
+++ main/views.py
@@ -52,13 +52,14 @@ def login_view(request):
     return render(request, 'main/login.html')
 
 def register_view(request):
-    if 'user_id' in request.session:
-        return redirect('main:home')
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
             try:
                 pengguna = form.save()
+                if 'user_id' in request.session:
+                    del request.session['user_id']
+                request.session.flush()
                 messages.success(request, 'Registration successful! Please log in.')
                 return redirect('main:login')
             except Exception as e:
